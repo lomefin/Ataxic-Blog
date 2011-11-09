762bf00d192f9c117a72a8e8527e22c60ac062df
Leonardo Luarte <leonardo@luarte.net>
2011-11-09 14:06:43 -0300

    i

---
+++ lib/slugify.py
@@ -0,0 +1,21 @@
+import re
+
+class Slugger():
+
+  @staticmethod
+  def slugify(value):
+      """
+      Normalizes string, converts to lowercase, removes non-alpha characters,
+      and converts spaces to hyphens.
+      
+      From Django's "django/template/defaultfilters.py".
+      """
+      _slugify_strip_re = re.compile(r'[^\w\s-]')
+      _slugify_hyphenate_re = re.compile(r'[-\s]+')
+
+      import unicodedata
+      if not isinstance(value, unicode):
+	  value = unicode(value)
+      value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+      value = unicode(_slugify_strip_re.sub('', value).strip().lower())
+      return _slugify_hyphenate_re.sub('-', value)
