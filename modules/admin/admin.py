dd2651267c575bad7f7ed4f4714fd81f038c1df2
Leonardo Luarte González <leonardo@luarte.net>
2011-12-08 22:14:06 -0300

    Se agregan mas handlers en admin para eliminar y cambiar estado de PostedElements, que se hizo generico. Se está intentando cambiar los nombres de los objetos de Posts a Articles.

--- modules/admin/admin.py
+++ modules/admin/admin.py
@@ -33,7 +33,7 @@ from lib import slugify
 from model.models import *
 from lib import markdown2
 
-class PostAdmin(llhandler.LLGAEHandler):
+class ArticleAdmin(llhandler.LLGAEHandler):
 	def base_directory(self):
 		return os.path.dirname(__file__)
 
@@ -45,9 +45,36 @@ class PostAdmin(llhandler.LLGAEHandler):
 	def internal_post(self):
 		self.render('post_list')
 
+class PostedElementDeleteConfirmation(llhandler.LLGAEHandler):
+	def base_directory(self):
+		return os.path.dirname(__file__)
+	
+	def internal_get(self,element_type,key):
+		element_key = LLPostedElement.get(Key(encoded=key))
+		values = { 'element' : element_key, 'element_type':element_type}
+		self.render('confirm_delete',template_values=values)
+	
+	def get(self,element_type,key):
+		self.auth_check()
+		self.internal_get(element_type,key)
+
+class PostedElementActiveStatusModifier(llhandler.LLGAEHandler):
+	def base_directory(self):
+		return os.path.dirname(__file__)
+	
+	def internal_get(self,element_type,key):
+		element_key = LLPostedElement.get(Key(encoded=key))
+		values = { 'element' : element_key, 'element_type':element_type}
+		self.render('confirm_delete',template_values=values)
+	
+	def get(self,element_type,key):
+		self.auth_check()
+		self.internal_get(element_type,key)
 
 def main():
-  application = webapp.WSGIApplication([('/admin/posts/.*', PostAdmin),
+  application = webapp.WSGIApplication([('/admin/articles/', ArticleAdmin),
+  										('/admin/(articles|news|link)/(.+?)/remove',PostedElementDeleteConfirmation),
+  										('/admin/(articles|news|link)/(.+?)/(activate|deactivate)',PostedElementActiveStatusModifier),
   										('.*',lib.errors.NotFoundHandler)],
                                        debug=True)
   util.run_wsgi_app(application)
